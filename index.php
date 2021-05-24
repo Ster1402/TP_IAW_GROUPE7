fd74c7230e3fe56141ebbc405f7fbe16e444a98e
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2021-05-24 18:23:14 +0100

    Squellette projet

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Cities</title>
+</head>
+<body>
+
+    <div id="root">
+
+        <header role="header" id="header">
+
+            <nav id="menu"></nav>
+            <div id="banniere"></div>
+
+        </header>
+        
+        <main id="container">
+
+            <div id="slider">
+                <!--Ici l'on doit gérer les sliders-->
+
+            </div>
+        
+        </main>
+
+        <footer id="footer">
+
+            <div class="address"></div>
+            <div class="copyright"></div>
+            <div class="contact"></div>
+
+        </footer>
+
+    </div>
+    
+</body>
+</html>
